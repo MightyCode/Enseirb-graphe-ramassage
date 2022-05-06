2edd9c39db03c1316120e29dde083a2d7529eb33
MightyCode <bazinmaxence@free.fr>
2022-05-06 10:20:43 +0200

    add basic methods

---
+++ pmath.py
@@ -0,0 +1,9 @@
+
+from xmlrpc.client import Boolean
+
+
+def rectangleCollidesLine(line: list, rectangle: list) -> bool:
+    return False
+
+def lineCollidesLine(x1, y1, x2, y2, z1, w1, z2, w2) -> bool:
+    return False
